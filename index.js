057b551a259933f1f890fb3926db9d6eb9292981
Josekariz <joseph.macharia@student.moringaschool.com>
2022-08-15 14:58:34 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser= 'Jos';
+let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+const shortGreeting= `Welcome, ${currentUser[0]}!`
